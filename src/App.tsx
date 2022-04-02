44c2ddd05e87fa42329704abc181754b5e88e663
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-04-02 19:09:30 -0300

    chore(*): Add CSS in JS & Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <h1>START</h1>
+      <GlobalStyle />
     </div>
   );
 }
