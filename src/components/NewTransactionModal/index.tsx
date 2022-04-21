8dc5b64fa5f7bbcf201ac4499ab8bb5bb7c7ff32
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-04-20 21:14:59 -0300

    feat: Add buttons and styles on Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 Modal.setAppElement('#root');
 
@@ -13,6 +16,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -31,7 +36,29 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
 
         <input placeholder='Título' />
-        <input placeholder='Valor' />
+        <input type='number' placeholder='Valor' />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type='button'
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor='green'
+          >
+            <img src={incomeImg} alt='Entrada' />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type='button'
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor='red'
+          >
+            <img src={outcomeImg} alt='Saída' />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
         <input placeholder='Categoria' />
 
         <button type='submit'>Cadastrar</button>
